94aaff1b8ad68d3dc5f38fd427a1e902d2b7b485
Brandon Tseng <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-06-27 16:32:43 -0700

    Updated and populated About me page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,52 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <h1>About this app</h1>
+    <p class="about-paragraph-1">
+      <span
+        >This is a simple todo app built with Vue 3 and the Composition API
+        (Reactivity API).</span
+      >
+    </p>
+    <p class="about-paragraph-2">
+      <span>
+        This app is based off of the
+        <a href="https://www.youtube.com/watch?v=KTFH4P8unUQ"
+          >Vue 3 Crash Course | Project From Scratch</a
+        >
+        by
+        <a href="https://www.youtube.com/@JohnKomarnicki">John Komarnicki</a>.
+      </span>
+    </p>
+  </main>
 </template>
 
-<style>
-  @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
+<style scoped lang="scss">
+  main {
+    display: flex;
+    flex-direction: column;
+    max-width: 800px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 40px 16px;
+
+    h1 {
+      margin-bottom: 16px;
+      text-align: center;
+    }
+
+    .about-paragraph-1 {
       display: flex;
-      align-items: center;
+      flex-direction: column;
+      list-style: none;
+      gap: 20px;
+    }
+
+    .about-paragraph-2 {
+      display: flex;
+      flex-direction: column;
+      list-style: none;
+      margin-top: 24px;
+      gap: 20px;
     }
   }
 </style>
